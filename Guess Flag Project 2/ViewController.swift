1cfcaa3b7fc0a138230ee097829354a1482c31a3
Gökberk Ali Kulaç <gokberkkulac@gmail.com>
2022-11-06 15:12:24 +0300

    Initial Commit

---
+++ Guess Flag Project 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Guess Flag Project 2
+//
+//  Created by Gökberk Ali Kulaç on 6.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
